b7c46fefb24f3584f355d6d97d32cc425b167229
lperezp <luiseperezp.10@gmail.com>
2019-12-24 01:21:25 -0500

    feat(function): add getHorse

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,10 +1,24 @@
 import * as functions from 'firebase-functions';
+import admin = require('firebase-admin');
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
+var serviceAccount = require("./serviceAccountKey.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://examples-cloud-functions.firebaseio.com"
+});
+
+const db = admin.firestore();
+
  export const helloWorld = functions.https.onRequest((request, response) => {
   response.json({
       mensaje :"Hola mundo desde Firebase!!"
     });
  });
+
+ export const getHorses = functions.https.onRequest( async (request, response) => {
+  const horseRef = db.collection('horses');
+  const docsSnap = await horseRef.get();
+  const horses = docsSnap.docs.map(doc => doc.data());
+  response.json(horses);
+});
